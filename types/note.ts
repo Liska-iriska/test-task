40620912773675ac4f4d3929912d659dbaed96aa
Lis <genia.grishenko@gmail.com>
2026-07-15 22:27:03 +0300

    Note.ts configuration

---
+++ types/note.ts
@@ -0,0 +1,10 @@
+export type NoteTag = "All" | "Done" | "Undone";
+
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  tag: NoteTag;
+  createdAt: string;
+  updatedAt: string;
+}
