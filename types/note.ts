f49d76b2771bb19cc84e564042db9c55751ed0b8
Lis <genia.grishenko@gmail.com>
2026-07-15 22:41:46 +0300

    API integration

--- types/note.ts
+++ types/note.ts
@@ -1,10 +1,11 @@
-export type NoteTag = "All" | "Done" | "Undone";
+export type NoteTag = "Done" | "Undone";
 
 export interface Note {
   id: string;
   title: string;
   content: string;
   tag: NoteTag;
+  rate: number;
   createdAt: string;
   updatedAt: string;
 }
