b2b42b5455776b4756ff15daa1e03132cf79d686
Lis <genia.grishenko@gmail.com>
2026-07-16 11:51:42 +0300

    Finish Noteform

---
+++ components/NoteForm/NoteForm.tsx
@@ -0,0 +1,102 @@
+"use client";
+import css from "./NoteForm.module.css";
+import type { NoteTag } from "../../types/note";
+import { useMutation } from "@tanstack/react-query";
+import { createNote } from "../../lib/api";
+import { useRouter } from "next/navigation";
+import { useNoteDraftStore } from "@/lib/store/noteStore";
+
+export default function NoteForm() {
+  const router = useRouter();
+  const { draft, setDraft, clearDraft } = useNoteDraftStore();
+
+  const handleChange = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >,
+  ) => {
+    setDraft({
+      ...draft,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const mutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      clearDraft();
+      router.push("/notes/filter/all");
+    },
+  });
+
+  const handleFormAction = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+    const tag = formData.get("tag") as NoteTag;
+    const rate = Number(formData.get("rate"));
+
+    mutation.mutate({ title, content, tag, rate });
+  };
+
+  const handleCancel = () => router.push("/notes/filter/all");
+
+  return (
+    <form action={handleFormAction} className={css.form}>
+      <div className={css.formGroup}>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          className={css.input}
+          defaultValue={draft?.title}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className={css.formGroup}>
+        <label htmlFor="content">Content</label>
+        <textarea
+          id="content"
+          name="content"
+          rows={8}
+          className={css.textarea}
+          defaultValue={draft?.content}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className={css.formGroup}>
+        <label htmlFor="tag">Tag</label>
+        <select
+          id="tag"
+          name="tag"
+          className={css.select}
+          defaultValue={draft?.tag}
+          onChange={handleChange}
+          required
+        >
+          <option value="Done">Todo</option>
+          <option value="Undone">Work</option>
+        </select>
+      </div>
+
+      <div className={css.actions}>
+        <button
+          type="button"
+          className={css.cancelButton}
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className={css.submitButton}
+          disabled={mutation.isPending}
+        >
+          {mutation.isPending ? "Creating..." : "Create a note"}
+        </button>
+      </div>
+    </form>
+  );
+}
